a60856206fbf6ad755a11e83d3d8ad3243833dfb
som2303 <joosomi@naver.com>
2019-10-30 17:52:00 +0900

    ok

---
+++ binary search tree.py
@@ -0,0 +1,156 @@
+class Node(object):
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+class BinarySearchTree(object):
+    def __init__(self):
+        self.root = None
+
+    def insert(self, data):
+        self.root = self._insert_value(self.root, data)
+        return self.root is not None
+
+    def _insert_value(self, node, data):
+        if node is None:
+            node = Node(data)
+        else:
+            if data <= node.data:
+                node.left = self._insert_value(node.left, data)
+            else:
+                node.right = self._insert_value(node.right, data)
+        return node
+
+
+    def find(self, key):
+        return self._find_value(self.root, key)
+
+    def _find_value(self, root, key):
+        if root is None or root.data == key:
+            return root is not None
+        elif key < root.data:
+            return self._find_value(root.left, key)
+        else:
+            return self._find_value(root.right, key)
+
+    def delete(self, key):
+        self.root, deleted = self._delete_value(self.root, key)
+        return deleted
+
+    def _delete_value(self, node, key):
+        if node is None:
+            return node, False
+
+        deleted = False
+        if key == node.data:
+            deleted = True
+            if node.left and node.right:
+                # replace the node to the leftmost of node.right
+                parent, child = node, node.right
+                while child.left is not None:
+                    parent, child = child, child.left
+                child.left = node.left
+                if parent != node:
+                    parent.left = child.right
+                    child.right = node.right
+                node = child
+            elif node.left or node.right:
+                node = node.left or node.right
+            else:
+                node = None
+        elif key < node.data:
+            node.left, deleted = self._delete_value(node.left, key)
+        else:
+            node.right, deleted = self._delete_value(node.right, key)
+        return node, deleted
+
+array = [40, 4, 34, 45, 14, 55, 48, 13, 15, 49, 47]
+
+bst = BinarySearchTree()
+for x in array:
+    bst.insert(x)
+
+# Find
+print(bst.find(15)) # True
+print(bst.find(17)) # False
+
+# Delete
+print(bst.delete(55)) # True
+print(bst.delete(14)) # True
+print(bst.delete(11)) # False
+'''
+
+class Node(object):
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+class BinarySearchTree(object):
+    def __init__(self):
+        self.root = None
+
+    def insert(self,node, data):
+        while True:
+            if self.root==None:
+                self.root=data
+                break
+            elif self.root==data:
+                break
+            elif self.root>data:
+                if self.left<data:
+                    l=self.left
+                    self.left=data
+                    self.left.left=l
+                    break
+                else:
+                    self.root=self.left
+            else:
+                if self.right < data:
+                    r = self.right
+                    self.right = data
+                    self.right.right = r
+                    break
+                else:
+                    self.root = self.right
+
+    def search(self, key):
+        while True:
+            if self.root==None:
+                return None
+            elif self.root==key:
+                return self.root
+            elif self.root>key:
+                self.root=self.left
+            else:
+                self.root=self.right
+
+    def delete(self,data):
+        f=self.search(data)
+        if f.right==None and f.left==None:
+            f=None
+        elif f.right==None:
+            f=f.left
+        elif f.left==None:
+            f=f.right
+        else:
+            f=f.left
+            while True:
+                f.right=f.right.right
+
+array = [40, 4, 34, 45, 14, 55, 48, 13, 15, 49, 47]
+
+bst = BinarySearchTree()
+for x in array:
+    bst.insert(x)
+
+# Find
+print(bst.search(15)) # True
+print(bst.search(17)) # False
+
+# Delete
+#print(bst.delete(55)) # True
+#print(bst.delete(14)) # True
+#print(bst.delete(11)) # False
+'''
